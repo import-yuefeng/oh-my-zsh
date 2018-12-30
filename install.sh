ec64e34dc428bf70524bfcc2de3d1aad3bfc0868
import-yuefeng <zhuyuefeng0@outlook.com>
2018-12-31 01:19:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/import-yuefeng/fonts.git
+git clone https://github.com/import-yuefeng/fonts.git ~/
 
 cd fonts && ./install.sh
 
