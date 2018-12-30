a13ebca7a9c34a919f48160adb2be9a91fccc3cf
import-yuefeng <zhuyuefeng0@outlook.com>
2018-12-31 01:17:54 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/import-yuefeng/fonts.git
+
+cd fonts && ./install.sh
+
+cd .. && echo export LC_POWERLINE_FONT=true > .userrc
+
+rm -rf .oh-my-zsh && cp -r oh-my-zsh .oh-my-zsh
+
+chsh -s /bin/zsh
+
+zsh
