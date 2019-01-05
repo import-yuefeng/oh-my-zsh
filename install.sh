ec5777402356a1e4b7f886fc26f6c4f8180cd188
import-yuefeng <git@gansi.me>
2019-01-05 23:23:11 +0800

    update install.sh
      - update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 git clone https://github.com/import-yuefeng/fonts.git ~/fonts
 
-cd ~/fonts && ./install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cd ~/ && echo "export LC_POWERLINE_FONT=true; export ZSH=~/.oh-my-zsh;" > .userrc
+sh -c ~/fonts/install.sh
+
+echo "export LC_POWERLINE_FONT=true; export ZSH=~/.oh-my-zsh;" > ~/.userrc
 
 rm -rf .oh-my-zsh && cp -r oh-my-zsh .oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-yuefeng ~/.zshrc
