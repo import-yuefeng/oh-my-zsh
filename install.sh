75032fb9fb478620569924abc06b3786f3ccb531
import-yuefeng <git@gansi.me>
2019-01-06 18:10:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
+# /bin/sh
+set -ue
+
 git clone https://github.com/import-yuefeng/fonts.git ~/fonts
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sh -c ~/fonts/install.sh
+chmod +x ~/fonts/install.sh && sh -c ~/fonts/install.sh
 
 echo "export LC_POWERLINE_FONT=true; export ZSH=~/.oh-my-zsh;" > ~/.userrc
 
-rm -rf .oh-my-zsh && cp -r oh-my-zsh .oh-my-zsh
+rm -rf .oh-my-zsh && cp -r ../oh-my-zsh ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-yuefeng ~/.zshrc
 
-chsh -s /bin/zsh
+chsh  /bin/zsh
 
 source ~/.zshrc
